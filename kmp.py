585e0a5c53c426e675d11db92ef42773481e08b4
Charlie Harding <xsanda@me.com>
2018-01-24 20:19:24 +0000

    Tidy and document the KMP algorithm

--- kmp.py
+++ kmp.py
@@ -1,65 +1,108 @@
-from sys import argv
+from sys import argv, exit
 from comparer import Comparer
 
 
-def prefix(pattern):
-    table = [0]
-    j = 0
-    compare = Comparer()
-    for i in range(1,len(pattern)):
-        print(table)
-        # if the last case was a success (j > 0), use it: if they compare true,
-        # that means that this character is the one after the prefix in the
-        # previous case
-        # If they compare false, then clearly this is not the case. However,
-        # we know that the past j characters are the same as the first j
-        # characters, so use the analysis of the first j characters. Keep checking
-        while j > 0 and not compare(pattern, i, pattern, j):
-            j = table[j-1]
+def prefix(pattern, compare):
+    """
+    Build the precalculated prefix table: For any index i, gives the length j
+    of the longest strict prefix pattern[0:j] which is also a strict suffix
+    pattern[i-j:i].
+    This is done iteratively, as each prefix-suffix builds on the previous one,
+    or a prefix thereof.
+    """
+    
+    # For the string of length 0, there is no prefix.
+    # For the string of length 1, a strict prefix must have length 0.
+    table = [None, 0]
 
-        # If j > 0, no need to perform character comparison: it must be true
-        # because while loop has terminated
+    # Initially there is no prefix-suffix to build on.
+    j = 0
+
+    # For each new character, excluding the initial character:
+    for i in range(1,len(pattern)):
+        # if the last prefix-suffix was a non-empty (j > 0), try to build upon
+        # it:
+            # If the character following the prefix is equal to the new
+            # character, then we can extend the prefix-suffix.
+            # Otherwise, we cannot extend the prefix-suffix. However, we know
+            # that the past j characters are the same as the first j
+            # characters, so use the analysis of the first j characters. Keep
+            # checking prefix-suffixes thereof until a match is found, or there
+            # is no more prefix-suffix left
+        while j > 0 and not compare(pattern, i, pattern, j):
+            j = table[j]
+
+        # If j > 0, no need to perform character comparison: comparison must
+        # hold because the loop terminated. Otherwise check if current
+        # character matches first in the pattern, starting a new prefix-suffix.
         if j > 0 or compare(pattern, i, pattern, j):
             j += 1
 
-        # j = 0: no prefix.
-        # j = 1: new prefix (len 1)
-        # j > 1: extension of previous prefix
+        # j = 0: no prefix-suffix
+        # j = 1: new prefix-suffix (length 1)
+        # j > 1: extension of previous prefix-suffix
         table.append(j)
     return table
 
-if __name__ == "__main__":
+
+def run_kmp(table, text, pattern, compare):
+    """
+    Using the precalculated prefix table, yield every match of the pattern in
+    the text, making comparisons with the provided compare function.
+    """
+
+    # Start with the first character of the text
+    i = 0
+
+    # And the first character of the pattern
+    j = 0
+
+    # While the pattern does not overflow the text:
+    while len(pattern) - j <= len(text) - i:
+        
+        # Check if the next character in the text matches the next in the
+        # pattern. If not, see if we can use a suffix of the text with a prefix
+        # of the pattern
+        while j > 0 and not compare(text, i, pattern, j):
+            j = table[j]
+
+        # If j > 0, no need to perform character comparison: comparison must
+        # hold because the loop terminated. Otherwise check if current
+        # character matches first in the pattern, starting a new match.
+        if j > 0 or compare(text, i, pattern, j):
+            j += 1
+
+        # If the whole pattern has been matched, return it, and reuse a suffix
+        # of the pattern for the next round.
+        if j == len(pattern):
+            yield i-j+1
+            j = table[j]
+
+        # Move on to the next character in the text
+        i += 1
+
+if __name__ == '__main__':
     try:
         pattern = argv[1]
-    except IndexError:
-        raise RuntimeError("No pattern")
-    try:
         text = argv[2]
     except IndexError:
-        raise RuntimeError("No text")
+        print('usage: python3 kmp.py PATTERN TEXT')
+        exit()
     
-    print('Searching for "'+pattern+'" in "'+text+'".')
+    print(f'Searching for "{pattern}" in "{text}".')
     print()
 
     compare = Comparer()
 
-    table = prefix(pattern)
-    print(table)
+    table = prefix(pattern, compare)
+    print(f'Prefix table: {table}')
+    print(f'Prefix table took {compare.count} comparisons to build')
+    print()
 
-    q = 0
+    # Use a new comparer for running the algorithm.
+    compare = Comparer()
 
-    for i in range(len(text)):
-        # omit this to implement the algorithm a la CLRS/the slides/sheet 7q1,
-        # which involves surplus comparisons
-        if i - q + len(pattern) > len(text):
-            break
+    for match in run_kmp(table, text, pattern, compare):
+        print(f'Match found at position {match}')
 
-        while q > 0 and not compare(text, i, pattern, q):
-            q = table[q-1]
-        if q > 0 or compare(text, i, pattern, q):
-            q += 1
-        if q == len(pattern):
-            print("Match found at position "+str(i-q+1))
-            q = table[q-1]
-
-    print(str(compare.count)+" comparisons")
+    print(f'{compare.count} comparisons')
